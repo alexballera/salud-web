ba45e9e921caa36150e6c573579a48422234bf4c
FedericoLeivaOMNI <78227112+FedericoLeivaOMNI@users.noreply.github.com>
2021-05-26 13:11:28 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
 import Head from 'next/head'
 import Image from 'next/image'
-=======
 import * as React from 'react'
->>>>>>> 484d47e184f2e68712ccf649cd5adf632b3ee85a
 import styles from '../styles/Home.module.scss'
 
 export default function Home() {
