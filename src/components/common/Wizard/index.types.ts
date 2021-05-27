2333711486778845822ebdb83aea190ced8a50b4
Wilmer Salazar <wilmer.salazar@omni.cr>
2021-05-27 14:03:52 -0300

    Wizard component

---
+++ src/components/common/Wizard/index.types.ts
@@ -0,0 +1,18 @@
+/// TYPES
+export type IWizardProps = {
+  dataSource: IWizardDataSourceItem[]
+}
+
+export type ITabPanelProps = {
+  dir?: string
+  index: number
+  value: number
+  data?: IWizardDataSourceItem
+}
+
+export type IWizardDataSourceItem = {
+  title: string
+  component: JSX.Element
+  description: string
+}
+/// TYPES END
