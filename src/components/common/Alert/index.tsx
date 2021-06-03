5200fdbc1535c4a0d306176b2c7ff36f940c1649
Federico Leiva <federicoleiva@MBP-de-Federico.fibertel.com.ar>
2021-06-03 10:56:24 -0300

    SLD-417 Themed <Alert /> bgcolors

--- src/components/common/Alert/index.tsx
+++ src/components/common/Alert/index.tsx
@@ -1,11 +1,12 @@
 import { Box } from '@material-ui/core';
 import React, { PropsWithChildren } from 'react';
+import { errorColor, successColor } from '../../../utils/theme';
 import { IProps } from './types';
 
 export default function Alert({ children, variant }: PropsWithChildren<IProps>): JSX.Element {
   return (
     <Box
-      bgcolor={variant === 'success' ? 'lime' : '#ff6666'}
+      bgcolor={variant === 'success' ? successColor : errorColor}
       padding="10px"
       borderRadius="5px"
       color="white"
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,12 +1,15 @@
-import pink from '@material-ui/core/colors/pink'
-import indigo from '@material-ui/core/colors/indigo'
-import { createMuiTheme } from '@material-ui/core/styles'
+import pink from '@material-ui/core/colors/pink';
+import indigo from '@material-ui/core/colors/indigo';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const errorColor = '#ff6666';
+export const successColor = '#4dff4d';
 
 const theme = createMuiTheme({
   palette: {
     primary: indigo,
     secondary: pink
   }
-})
+});
 
-export default theme
+export default theme;
