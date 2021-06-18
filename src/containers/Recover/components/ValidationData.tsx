5dbda7fded05cb9763270dd08e3fb3188d52a572
FedericoLeivaOMNI <78227112+FedericoLeivaOMNI@users.noreply.github.com>
2021-06-18 10:48:17 -0300

    subtask/SLD-537 unregistred email modal (#20)
    Co-authored-by: Federico Leiva <federicoleiva@MBP-de-Federico.fibertel.com.ar>

--- src/containers/Recover/components/ValidationData.tsx
+++ src/containers/Recover/components/ValidationData.tsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FormikProps } from 'formik';
 import * as yup from 'yup';
 /// TYPES
 import { IValidationDataForm } from '../../../types/recover.types';
 /// MATERIAL-UI
 import FormControl from '@material-ui/core/FormControl';
-import { Box, makeStyles } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Slide,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  makeStyles
+} from '@material-ui/core';
 import ReactCodeInput from 'react-code-input';
 import {
   forgotPasswordConfirmCodeService,
@@ -19,7 +29,12 @@ interface IProps extends FormikProps<IValidationDataForm> {
   handleError: (open: boolean, message?: string) => void;
 }
 
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 function ValidationData({ values, handleChange, handleLoading, handleError }: IProps): JSX.Element {
+  const [dialogOpen, setDialogOpen] = useState(false);
   const router = useRouter();
   const classes = useStyles();
 
@@ -27,12 +42,16 @@ function ValidationData({ values, handleChange, handleLoading, handleError }: IP
     handleLoading(true);
     forgotPasswordSendEmailService(values.email)
       .catch(err => {
-        if (err.response) handleError(true, err.response.data.error.message);
-        else
+        if (err.response && err.response.status === 404) {
+          return setDialogOpen(true);
+        } else if (err.response) {
+          handleError(true, err.response.data.error.message);
+        } else {
           handleError(
             true,
             'Ha ocurrido un error desconocido. Vuelve a intentarlo o contacta a un administrador.'
           );
+        }
         router.back();
       })
       .finally(() => {
@@ -76,19 +95,45 @@ function ValidationData({ values, handleChange, handleLoading, handleError }: IP
   }, [values.pinCode]);
 
   return (
-    <Box>
-      <FormControl fullWidth margin="normal" className={classes.root}>
-        <ReactCodeInput
-          type="text"
-          name="pinCode"
-          fields={6}
-          inputMode="numeric"
-          value={values.pinCode}
-          onChange={handleChange('pinCode')}
-          disabled={values.validPin === '1'}
-        />
-      </FormControl>
-    </Box>
+    <>
+      <Box>
+        <FormControl fullWidth margin="normal" className={classes.root}>
+          <ReactCodeInput
+            type="text"
+            name="pinCode"
+            fields={6}
+            inputMode="numeric"
+            value={values.pinCode}
+            onChange={handleChange('pinCode')}
+            disabled={values.validPin === '1'}
+          />
+        </FormControl>
+      </Box>
+
+      <Dialog
+        open={dialogOpen}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={router.back}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">Correo electrónico no encontrado</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            El correo indicado no está registrado, ¿desea registrarse?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => router.back()} color="secondary">
+            Cancelar
+          </Button>
+          <Button onClick={() => router.replace('/signup')} color="primary">
+            Registrarse
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
 
