1ad1ea89e0e64c8c9d1d5ed5ecb18f685a42c8c4
WilmerEnisOmni <77505383+WilmerEnisOmni@users.noreply.github.com>
2021-07-02 10:39:23 -0300

    bug/SLD-602 (#40)

--- src/context/types.ts
+++ src/context/types.ts
@@ -4,7 +4,6 @@ export type AppStates = {
   loggedIn: boolean;
   fetching: boolean;
   errorState: { open: boolean; message: string; type: 'success' | 'error' | 'warning' };
-  handleNotifications: (props: INotificationProps) => void;
 };
 
 export type Props = {
