81930e7956aa2f33551f93b0534d158d33e43790
Alex Ballera <alexballera@gmail.com>
2021-09-22 00:03:13 -0300

    SLD-526 Se agrega avatar

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Divider, Grid, Hidden } from '@material-ui/core';
 import LayoutInner from '../components/common/LayoutInner';
 import { TitleProfile } from '../containers/Profile/TitleProfile';
@@ -7,9 +7,21 @@ import { CredentialsProfile } from '../containers/Profile/CredentialsProfile';
 import { LegalProfile } from '../containers/Profile/LegalProfile';
 import { PersonalProfile } from '../containers/Profile/PersonalProfile';
 import ProfileStyles from '../containers/Profile/styles.module';
+import { getPersonalData, IPersonalData } from '../services/getPersonalData.service';
+/// GET SERVICE END
 
 export default function ProfilePage(): JSX.Element {
   const classes = ProfileStyles();
+  const [fullName, setFullName] = useState('');
+  const [documentNumber, setDocumentNumber] = useState('');
+
+  getPersonalData('bastidasarelis2021@gmail.com')
+    .then(res => {
+      const personalData: IPersonalData = res.data.result;
+      setFullName(personalData.fullName);
+      setDocumentNumber(personalData.documentNumber);
+    })
+    .catch(err => console.log(err));
   return (
     <LayoutInner>
       <Grid container>
@@ -17,7 +29,7 @@ export default function ProfilePage(): JSX.Element {
           <TitleProfile />
         </Grid>
         <Grid item xs={12} md={5}>
-          <AvatarProfile />
+          <AvatarProfile fullName={fullName} documentNumber={documentNumber} />
           <Hidden mdUp>
             <Divider className={classes.divider} />
           </Hidden>
--- src/services/getPersonalData.service.ts
+++ src/services/getPersonalData.service.ts
@@ -1,5 +1,18 @@
 import axios, { AxiosResponse } from 'axios';
 /// TYPES
+export type IPersonalData = {
+  codSAC: number;
+  documentNumber: string;
+  documentType: number;
+  email: string;
+  emailId: number;
+  firstMedicalRecordComplete: number;
+  firstName: string;
+  fullName: string;
+  secondName: string;
+  status: string;
+  statusCode: number;
+};
 
 export const getPersonalData = (email: string): Promise<AxiosResponse<any>> => {
   return axios.post(`${process.env.NEXT_PUBLIC_API_URL}user/get-local-data`, {
