105a5a245b8cc40403a485cca7af2971bff27c28
Alex Ballera <alexballera@gmail.com>
2021-09-23 23:58:42 -0300

    SLD-527 Se agregan datos en appbar

--- src/components/common/Navbar/styles.module.tsx
+++ src/components/common/Navbar/styles.module.tsx
@@ -9,6 +9,16 @@ const navbarStyles = makeStyles({
   },
   toolbarDesktop: {
     backgroundColor: 'rgb(250, 250, 250)'
+  },
+  name: {
+    color: 'rgba(0, 0, 0, 0.87)',
+    fontSize: 16,
+    fontWeight: 'normal'
+  },
+  documentNumber: {
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontSize: 14,
+    fontWeight: 'normal'
   }
 });
 
