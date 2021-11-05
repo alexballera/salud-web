261e976179a28f7b708d454b8dcab8defa23d783
Alex Ballera <alexballera@gmail.com>
2021-11-05 01:10:57 -0300

    SLD-526 maquetado card

--- src/containers/Subscription/SubscriptionPlan.tsx
+++ src/containers/Subscription/SubscriptionPlan.tsx
@@ -1,45 +1,116 @@
 import React from 'react';
 import {
+  Box,
   Button,
   Card,
   CardActions,
   CardContent,
+  Chip,
+  createStyles,
   Divider,
+  Grid,
   makeStyles,
+  Theme,
   Typography
 } from '@material-ui/core';
-
-const useStyles = makeStyles({
-  root: {
-    backgroundColor: 'rgb(0, 151, 167)',
-    minWidth: 275
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  }
-});
+import SvgContainer from '../../components/common/SvgContainer';
+import SvgSubscription from '../../components/common/Svg/SvgSubscription.component';
+import SvgSubscriptionBottom from '../../components/common/Svg/SvgSubscriptionBottom.component';
+import AutorenewIcon from '@material-ui/icons/Autorenew';
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      backgroundColor: 'rgb(0, 151, 167)',
+      minWidth: 275
+    },
+    cardContent: {
+      paddingTop: '0px !important'
+    },
+    chip: {
+      fontSize: 14
+    },
+    containerText: {
+      alignItems: 'end'
+    },
+    text: {
+      order: 1,
+      [theme.breakpoints.up('sm')]: {
+        order: 0
+      }
+    },
+    containerSvg: {
+      textAlign: 'right',
+      width: '100%'
+    },
+    containerActions: {
+      padding: 16
+    },
+    divider: {
+      marginBottom: '0.35em'
+    },
+    title: {
+      color: 'white',
+      marginTop: '0.35em'
+    },
+    description: {
+      color: 'rgb(255, 255, 255)',
+      fontWeight: 'normal',
+      fontSize: 12
+    },
+    colorWhite: {
+      color: 'white'
+    }
+  })
+);
 
 const SubscriptionPlan = (): JSX.Element => {
   const classes = useStyles();
   return (
-    <Card className={classes.root}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-        </Typography>
+    <>
+      <Card className={classes.card}>
+        <CardContent className={classes.cardContent}>
+          <Grid container justify="space-between" className={classes.containerText}>
+            <Grid item sm={2} className={classes.text}>
+              <Chip size="small" label="Plan actual" color="secondary" className={classes.chip} />
+            </Grid>
+            <Grid item sm={10} className={classes.containerSvg}>
+              <Box>
+                <SvgContainer title="Subscription Svg" width={227} height={179}>
+                  <SvgSubscription />
+                </SvgContainer>
+              </Box>
+            </Grid>
+          </Grid>
 
-        <Divider />
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+          <Typography variant="h6" component="h6" gutterBottom className={classes.title}>
+            Orientación médica desde donde estés
+          </Typography>
+
+          <Divider className={classes.divider} />
+
+          <Typography variant="h6" component="h6" gutterBottom className={classes.description}>
+            Modalidad (individual)
+          </Typography>
+
+          <Typography variant="h6" component="h6" gutterBottom className={classes.description}>
+            Próxima fecha de pago: 23 de Abril
+          </Typography>
+        </CardContent>
+        <CardActions className={classes.containerActions}>
+          <Button
+            variant="outlined"
+            color="primary"
+            className={classes.colorWhite}
+            startIcon={<AutorenewIcon className={classes.colorWhite} />}
+          >
+            Cambiar de plan
+          </Button>
+        </CardActions>
+      </Card>
+      <SvgContainer title="Subscription Bottom Svg" width={140} height={116}>
+        <SvgSubscriptionBottom />
+      </SvgContainer>
+    </>
   );
 };
 
