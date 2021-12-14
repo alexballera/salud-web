27e2146e563221f6392f82ee14003c6b834ce8a0
Alex Ballera <alexballera@gmail.com>
2021-12-13 21:47:06 -0300

    SLD-815 Se corrigen estilos de input datepicker

--- src/styles/js/theme.ts
+++ src/styles/js/theme.ts
@@ -1,4 +1,4 @@
-import { createMuiTheme, ThemeOptions, Theme } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeOptions } from '@material-ui/core/styles';
 
 export const errorColor = '#ff6666';
 export const successColor = '#4caf50';
