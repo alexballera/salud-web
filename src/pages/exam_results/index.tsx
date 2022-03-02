574699a72f321e5d0cb35a8b46ca2fe058f8d7db
Alex Ballera <alexballera@gmail.com>
2022-03-02 19:23:14 -0300

    SLD-1200 se instala MUI v5

--- src/pages/exam_results/index.tsx
+++ src/pages/exam_results/index.tsx
@@ -1,5 +1,6 @@
 /// BASE IMPORTS
 import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
 /// BASE IMPORTS
 
 /// i18n
@@ -8,7 +9,7 @@ import { NAMESPACE_KEY as i18ExamResult } from '@/src/i18n/clinic_history/i18n';
 /// i18n END
 
 /// MUI COMPONENTS
-import { Box } from '@material-ui/core';
+import { Box, Tab, Tabs, Typography } from '@mui/material';
 /// MUI COMPONENTS END
 
 /// OWN COMPONENTS
@@ -17,14 +18,102 @@ import { withAppContext } from '@/src/context';
 
 /// STYLES
 import examResultStyles from '@/src/containers/ExamResult/styles.module';
+import { useTheme } from '@mui/material/styles';
 /// STYLES END
+
+/// TYPES
+interface TabPanelProps {
+  children?: React.ReactNode;
+  dir?: string;
+  index: number;
+  value: number;
+}
+/// TYPES END
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`
+  };
+}
 
 const ExamResult = (): JSX.Element => {
   const { t } = useTranslation(i18ExamResult);
+  const theme = useTheme();
   const classes = examResultStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  const handleChangeIndex = (index: number) => {
+    setValue(index);
+  };
+
   return (
     <Box p={3}>
-      <h2>Resultados de exámenes</h2>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons="auto"
+        aria-label="scrollable auto tabs example"
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+        <Tab label="Item Four" {...a11yProps(3)} />
+        <Tab label="Item Five" {...a11yProps(4)} />
+        <Tab label="Item Six" {...a11yProps(5)} />
+        <Tab label="Item Seven" {...a11yProps(6)} />
+      </Tabs>
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <TabPanel value={value} index={0} dir={theme.direction}>
+          Item One
+        </TabPanel>
+        <TabPanel value={value} index={1} dir={theme.direction}>
+          Item Two
+        </TabPanel>
+        <TabPanel value={value} index={2} dir={theme.direction}>
+          Item Three
+        </TabPanel>
+        <TabPanel value={value} index={3} dir={theme.direction}>
+          Item Four
+        </TabPanel>
+        <TabPanel value={value} index={4} dir={theme.direction}>
+          Item Five
+        </TabPanel>
+        <TabPanel value={value} index={5} dir={theme.direction}>
+          Item Six
+        </TabPanel>
+        <TabPanel value={value} index={6} dir={theme.direction}>
+          Item Seven
+        </TabPanel>
+      </SwipeableViews>
     </Box>
   );
 };
