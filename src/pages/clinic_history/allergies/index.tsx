077d7204252a98ec07480f72f4d032b81c671c44
Bryan Montero <bryanmontero.dev@gmail.com>
2022-03-03 12:08:19 -0600

    SLD-1027: moving styles and typing

--- src/pages/clinic_history/allergies/index.tsx
+++ src/pages/clinic_history/allergies/index.tsx
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 /// BASE IMPORTS
 
-/// TYPES
-import type { NextPageContext } from 'next/';
-/// / TYPES END
+/// STYLES
+import allergieStyles from './styles.module';
+/// STYLES END
 
 /// i18n
 import { useTranslation } from 'react-i18next';
@@ -17,68 +17,39 @@ import { Box, makeStyles, Typography, Card, Divider, Chip, Container } from '@ma
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 /// MATERIAL UI END
 
+/// TYPES
+import type { NextPageContext } from 'next/';
+
 type TAllergies = {
-    description: string;
+    name: string;
     isActive: boolean;
 };
+type TProps = {
+    allergies: TAllergies[];
+};
+/// / TYPES END
 
 
-const useStyles = makeStyles({
-    cardAllergies: {
-        borderRadius: 16,
-        boxShadow: '0px 4px 8px rgba(207, 225, 227, 0.5)',
-    },
-    contentButton: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        height: '56px',
-        margin: '8px 0px'
-    },
-    buttonText: {
-        width: '60%',
-        fontSize: '16px'
-    },
-    chipStatus: {
-        fontSize: '12px',
-        height: '20px'
-    },
-    chipActive: {
-        backgroundColor: '#BB9AFD1A',
-        color: '#AB82FF'
-    },
-    chipInative: {
-        backgroundColor: '#E4EBED',
-        color: '#829296'
-    },
-    typography14: {
-        fontSize: '14px'
-    },
-    typography16: {
-        fontSize: '16px'
-    },
-});
+const Allergies = ({ allergies }: TProps): JSX.Element => {
 
-const Allergies = ({ allergies }): JSX.Element => {
-
-    const classes = useStyles();
+    const classes = allergieStyles();
     const { t } = useTranslation(i18Allergies);
 
     return (
         <Container>
             <Box>
-                <Card className={classes.cardAllergies}>
+                <Card className={classes.cardAllergie}>
                     <Box mt={2} mx={2}>
                         <Typography paragraph color="secondary" className={classes.typography16}> {t('allergies', { ns: i18Allergies })} </Typography>
                     </Box>
                     <Divider variant="fullWidth" />
                     <Box mx={2}>
                         {
-                            allergies.sort((a, b) => a.description.localeCompare(b.description)).map((allergie, index) => (
+                            allergies.sort((a, b) => a.name.localeCompare(b.name)).map((allergie, index) => (
                                 <Box key={index}>
-                                    <Link href={`/clinic_history/allergies/${allergie.description}`}>
+                                    <Link href={`/clinic_history/allergies/${allergie.name}`}>
                                         <Box component="span" className={classes.contentButton}>
-                                            <Typography variant='body2' color="primary" className={classes.buttonText}> {allergie.description} </Typography>
+                                            <Typography variant='body2' color="primary" className={classes.buttonText}> {allergie.name} </Typography>
                                             <Chip label={allergie.isActive ? t('active',{ ns: i18Allergies }) : t('inactive',{ ns: i18Allergies }) } className={[classes.chipStatus, allergie.isActive ? classes.chipActive : classes.chipInative].join(' ')} />
                                             <ChevronRightIcon color="secondary" />
                                         </Box>
@@ -102,13 +73,13 @@ Allergies.getInitialProps = async ({ query }: NextPageContext) => {
 
     const allergies: TAllergies[] = [
         {
-            description: "Celiaquía",
+            name: "Celiaquía",
             isActive: false
         }, {
-            description: "Penicilina",
+            name: "Penicilina",
             isActive: true
         }, {
-            description: "Aleve",
+            name: "Aleve",
             isActive: true
         }
     ];
