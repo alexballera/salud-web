dd15b7ac865e2da56cf7bf607a8786676797c7b4
Bryan Montero <bryanmontero.dev@gmail.com>
2022-03-03 11:39:39 -0600

    SLD-1028: completion of allergy page detail

--- src/i18n/allergies/en.ts
+++ src/i18n/allergies/en.ts
@@ -2,5 +2,12 @@ export const enAllergies = {
     allergies: "Allergies",
     unregistered: "Unregistered",
     active: "Active",
-    inactive: "Inactive"
+    inactive: "Inactive",
+    detail: {
+        allergen: "Allergen",
+        doctor: "Doctor",
+        specialty: "Specialty",
+        description: "Description",
+        comments: "Comments"
+    }
 };
--- src/pages/clinic_history/allergies/[allergie_id].tsx
+++ src/pages/clinic_history/allergies/[allergie_id].tsx
@@ -1,33 +1,12 @@
 import React from 'react';
 
 /// MATERIAL UI
-import { Box, makeStyles, Typography, Card, Divider, Chip, Container } from '@material-ui/core';
+import { Box, makeStyles, Typography, Card, Divider, Chip, Container, Grid } from '@material-ui/core';
 /// MATERIAL UI END
 
-const useStyles = makeStyles({
-    cardAllergie: {
-        borderRadius: 16,
-        boxShadow: '0px 4px 8px rgba(207, 225, 227, 0.5)',
-    },
-    chipStatus: {
-        fontSize: '12px',
-        height: '20px'
-    },
-    chipActive: {
-        backgroundColor: '#BB9AFD1A',
-        color: '#AB82FF'
-    },
-    chipInative: {
-        backgroundColor: '#E4EBED',
-        color: '#829296'
-    },
-    typography14: {
-        fontSize: '14px'
-    },
-    typography16: {
-        fontSize: '16px'
-    },
-});
+/// STYLES & TYPES
+import allergieStyles from './styles.module';
+/// STYLES & TYPES END
 
 /// i18n
 import { useTranslation } from 'react-i18next';
@@ -38,24 +17,23 @@ import { NAMESPACE_KEY as i18Allergies } from '@/src/i18n/allergies/i18n';
 import type { NextPageContext } from 'next/';
 
 type TAllergie = {
-    time: string;
-    startDate: string;
-    endDate: string;
+    name: string;
     isActive: boolean;
     description: string;
     comments: string;
-    reportedBy: string;
     performer: string;
     specialization: string;
+};
+
+type TProps = {
+    allergie: TAllergie;
 };
 /// / TYPES END
 
 
+const AllergieDetail = ({ allergie }: TProps): JSX.Element => {
 
-
-const AllergieDetail = ({ allergie }): JSX.Element => {
-
-    const classes = useStyles();
+    const classes = allergieStyles();
     const { t } = useTranslation(i18Allergies);
 
     return (
@@ -65,7 +43,44 @@ const AllergieDetail = ({ allergie }): JSX.Element => {
             </Box>
             <Card className={classes.cardAllergie}>
                 <Box m={2}>
-                <Chip label={allergie.isActive ? t('active',{ ns: i18Allergies }) : t('inactive',{ ns: i18Allergies }) } className={[classes.chipStatus, allergie.isActive ? classes.chipActive : classes.chipInative].join(' ')} />
+                    <Grid container>
+                        <Grid item xs={12}>
+                            <Chip label={allergie.isActive ? t('active', { ns: i18Allergies }) : t('inactive', { ns: i18Allergies })} className={[classes.chipStatus, allergie.isActive ? classes.chipActive : classes.chipInative].join(' ')} />
+                            <Box mt={2} mb={2}>
+                                <Typography variant='body2' className={classes.colorTitle}>{t('detail.allergen', { ns: i18Allergies })}</Typography>
+                                <Grid item xs={12}>Gluten</Grid>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+                        <Grid item xs={12} className={classes.spacingRow}>
+                            <Typography variant='body2' className={classes.colorTitle}>{t('detail.doctor', { ns: i18Allergies })}</Typography>
+                            <Grid item xs={12}>{allergie.performer}</Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+                        <Grid item xs={12} className={classes.spacingRow}>
+                            <Typography variant='body2' className={classes.colorTitle}>{t('detail.specialty', { ns: i18Allergies })}</Typography>
+                            <Grid item xs={12}>{allergie.specialization}</Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+                        <Grid item xs={12} className={classes.spacingRow}>
+                            <Typography variant='body2' className={classes.colorTitle}>{t('detail.description', { ns: i18Allergies })}</Typography>
+                            <Grid item xs={12}>{allergie.description}</Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Divider />
+                        </Grid>
+                        <Grid item xs={12} className={classes.spacingRow}>
+                            <Typography variant='body2' className={classes.colorTitle}>{t('detail.comments', { ns: i18Allergies })}</Typography>
+                            <Grid item xs={12}>{allergie.comments}</Grid>
+                        </Grid>
+                    </Grid>
                 </Box>
             </Card>
         </Container>
@@ -75,37 +90,38 @@ const AllergieDetail = ({ allergie }): JSX.Element => {
 AllergieDetail.getInitialProps = async ({ query }: NextPageContext) => {
 
     const { allergie_id } = query;
-
     const allergies: TAllergie[] = [
         {
-            time: "2022-02-11T04:27:21.337Z",
-            startDate: "2020-10-08T04:27:21.337Z",
-            endDate: null,
+            name: "Penicilina",
             isActive: true,
-            description: "Penicilina",
+            description: "Se presenta hipersensibilidad a la benactizina después de las últimas semanas.",
             comments: "Reacción alérgica al paciente se visualiza en forma ronchas y enrojecimiento en la piel",
-            reportedBy: "MEDICO DEMO",
             performer: "Dr. Lorem Ipsum",
             specialization: "Allergologist"
         },
         {
-            time: "2021-10-18T04:27:21.337Z",
-            startDate: "1984-05-16T04:27:21.337Z",
-            endDate: "2021-10-18T04:27:21.337Z",
+            name: "Celiaquía",
             isActive: false,
-            description: "Celiaquía",
+            description: "Se presenta hipersensibilidad a la benactizina después de las últimas semanas.",
             comments: "Alergia al polvo, a la humedad, al frío",
-            reportedBy: "MEDICO DEMO",
+            performer: "Dr. Lorem Ipsum",
+            specialization: "Allergologist"
+        },
+        {
+            name: "Aleve",
+            isActive: true,
+            description: "Se presenta hipersensibilidad a la benactizina después de las últimas semanas.",
+            comments: "Alergia al polvo, a la humedad, al frío",
             performer: "Dr. Lorem Ipsum",
             specialization: "Allergologist"
         }
     ];
 
-    const allergie = allergies.find((allergie) => allergie.description === allergie_id);
+    const allergie = allergies.find((allergie) => allergie.name === allergie_id);
 
     return {
         allergie
     };
 };
 
-export default AllergieDetail
+export default AllergieDetail;
