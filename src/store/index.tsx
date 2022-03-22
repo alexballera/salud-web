35cd253c3783dcadbe80ac5da1a100159ef21614
Bryan Montero <bryanmontero.dev@gmail.com>
2022-03-22 16:46:24 -0600

    SLD-1398: add configurations store

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,8 +1,21 @@
+import { useDispatch as useReduxDispatch, useSelector as useReduxSelector } from 'react-redux';
+import type { TypedUseSelectorHook } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import type { Action, ThunkAction } from '@reduxjs/toolkit';
+
 import { apiBFF } from '../services/apiBFF';
 
-export const store = configureStore({
+const store = configureStore({
   reducer: {
     [apiBFF.reducerPath]: apiBFF.reducer
-  }
+  },
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiBFF.middleware),
+  devTools: process.env.NODE_ENV !== 'production'
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type AppThunk = ThunkAction<void, RootState, null, Action<string>>;
+export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
+export const useDispatch = (): unknown => useReduxDispatch<AppDispatch>();
+export default store;
