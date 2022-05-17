8635e73dca09391248a505825ca2fbf965ee721e
lilibethmaneiro <lilibeth.maneiro@omni.cr>
2022-05-16 22:56:42 -0300

    SLD-1817 fix height box

--- src/components/common/ModalFilters/index.tsx
+++ src/components/common/ModalFilters/index.tsx
@@ -255,7 +255,7 @@ const ModalFilters = ({ openModal, closeModal }: Tprops): JSX.Element => {
             </Box>
           </Grid>
           <Grid item xs={12} mt={3}>
-            <Box sx={{ height: 250 }}>
+            <Box>
               <Typography variant="caption" className={classes.titleFilter}>
                 {t('filters.name.distance', { ns: i18nMedicalDirectory })}
               </Typography>
